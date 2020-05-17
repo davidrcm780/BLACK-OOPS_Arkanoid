12611f19e4ce1da2d2ff8133484777df00c7ed17
DanielQuesada97 <00147619@uca.edu.sv>
2020-05-17 15:23:06 -0600

    Add: Initial Commit

---
+++ BLACK-OOPS_Arkanoid/BLACK-OOPS_Arkanoid/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿using System.Windows.Forms;
+
+namespace BLACK_OOPS_Arkanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.player);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Margin = new System.Windows.Forms.Padding(1);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(800, 450);
+            this.playground.TabIndex = 0;
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image) (resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(712, 23);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.player.Image = ((System.Drawing.Image) (resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(286, 370);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(199, 68);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.player)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.Timer timer1;
+
+        #endregion
+    }
+}
---
+++ BLACK-OOPS_Arkanoid/BLACK-OOPS_Arkanoid/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace BLACK_OOPS_Arkanoid
+{
+    public partial class Form1 : Form
+    {
+        //BALL SPEED
+        public int speedX = 5; 
+        public int speedY = 4;
+        //POINTS SCORED
+        public int point = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+            DoubleBuffered = true;
+            
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;     //REMOVES ANY BORDER
+            this.TopMost = true;                             //BRINGS THE FORM TO THE FRONT
+            this.Bounds = Screen.PrimaryScreen.Bounds;       //MAKES IT FULL SCREEN
+
+            player.Top = playground.Bottom - (playground.Bottom / 10);    //SETS PLAYER POSITION
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.Left = Cursor.Position.X - (player.Width / 2);    //THIS CENTERS THE PLAYER AROUND THE CURSOR
+            
+            //BALL MOVEMENT
+            ball.Left += speedX;
+            ball.Top += speedY;
+            
+            //COLLISION OF BALL AND PLAYER
+            if (ball.Bottom >= player.Top && ball.Bottom <= player.Bottom && ball.Left >= player.Left && ball.Right <= player.Right)
+            {
+                //speed_top += 2;
+                //speed_left += 2;
+                speedY = -speedY; //DIRECTION CHANGE
+                point += 1; //TO SAVE SCORE
+            }
+            
+            //COLLISIONS OF BALL AND PLAYGROUND
+            if (ball.Left <= playground.Left)
+            {
+                speedX = -speedX;
+            }
+
+            if (ball.Right >= playground.Right)
+            {
+                speedX = -speedX;
+            }
+
+            if (ball.Top <= playground.Top)
+            {
+                speedY = -speedY;
+            }
+
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;     //BALL IS OUT SO GAME STOPS
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();                //ESC TO QUIT
+        }
+    }
+}
