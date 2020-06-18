c3fdf705b5057e70d7ff94c7d031da3937d23d11
DanielQuesada97 <00147619@uca.edu.sv>
2020-06-18 15:30:09 -0600

    add: minor block update

---
+++ BLACK-OOPS_Arkanoid/BLACK-OOPS_Arkanoid/CustomPictureBox.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace BLACK_OOPS_Arkanoid
+{
+    public class CustomPictureBox : PictureBox
+    {
+        public int Golpes { get; set; }
+
+        public CustomPictureBox() : base() {}
+    }
+}
