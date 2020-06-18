f58b6f40bcf846fefdcb7dffc45aa4ac305edced
DanielQuesada97 <00147619@uca.edu.sv>
2020-06-18 17:51:50 -0600

    update

--- BLACK-OOPS_Arkanoid/BLACK-OOPS_Arkanoid/GameMenu.Designer.cs
+++ BLACK-OOPS_Arkanoid/BLACK-OOPS_Arkanoid/GameMenu.Designer.cs
@@ -46,21 +46,21 @@ namespace BLACK_OOPS_Arkanoid
             this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
             this.tableLayoutPanel1.Controls.Add(this.exitButton, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.scoresButton, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.playButton, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.79105F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.40298F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.40298F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.40298F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(891, 494);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(668, 401);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // exitButton
@@ -69,10 +69,10 @@ namespace BLACK_OOPS_Arkanoid
             this.exitButton.BackColor = System.Drawing.Color.Black;
             this.exitButton.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.exitButton.ForeColor = System.Drawing.Color.White;
-            this.exitButton.Location = new System.Drawing.Point(356, 420);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exitButton.Location = new System.Drawing.Point(267, 340);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(179, 50);
+            this.exitButton.Size = new System.Drawing.Size(134, 41);
             this.exitButton.TabIndex = 3;
             this.exitButton.Text = "EXIT";
             this.exitButton.UseVisualStyleBackColor = false;
@@ -83,10 +83,10 @@ namespace BLACK_OOPS_Arkanoid
             this.scoresButton.BackColor = System.Drawing.Color.Black;
             this.scoresButton.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.scoresButton.ForeColor = System.Drawing.Color.White;
-            this.scoresButton.Location = new System.Drawing.Point(356, 323);
-            this.scoresButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.scoresButton.Location = new System.Drawing.Point(267, 262);
+            this.scoresButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.scoresButton.Name = "scoresButton";
-            this.scoresButton.Size = new System.Drawing.Size(179, 50);
+            this.scoresButton.Size = new System.Drawing.Size(134, 41);
             this.scoresButton.TabIndex = 2;
             this.scoresButton.Text = "SCORES";
             this.scoresButton.UseVisualStyleBackColor = false;
@@ -96,8 +96,8 @@ namespace BLACK_OOPS_Arkanoid
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(245, 38);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(134, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(400, 130);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -110,10 +110,10 @@ namespace BLACK_OOPS_Arkanoid
             this.playButton.BackColor = System.Drawing.Color.Black;
             this.playButton.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.playButton.ForeColor = System.Drawing.Color.White;
-            this.playButton.Location = new System.Drawing.Point(356, 228);
-            this.playButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.playButton.Location = new System.Drawing.Point(267, 185);
+            this.playButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(179, 50);
+            this.playButton.Size = new System.Drawing.Size(134, 41);
             this.playButton.TabIndex = 1;
             this.playButton.Text = "PLAY";
             this.playButton.UseVisualStyleBackColor = false;
@@ -121,14 +121,14 @@ namespace BLACK_OOPS_Arkanoid
             // 
             // GameMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(891, 494);
+            this.ClientSize = new System.Drawing.Size(668, 401);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GameMenu";
             this.Text = "Arkanoid";
             this.tableLayoutPanel1.ResumeLayout(false);
